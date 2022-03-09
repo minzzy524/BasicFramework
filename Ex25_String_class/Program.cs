a780de7b454cb53f7bfa38495905d80b55649d24
MINSEONG KIM <58250610+minzzy524@users.noreply.github.com>
2022-03-09 19:59:32 +0900

    0309 20:00

---
+++ Ex25_String_class/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex25_String_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = "가";
+            string b = "나";
+            string c = a + b;
+            Console.WriteLine($"c : {c}");
+
+            string[] strarr = new string[] { "가", "나", "다", "라", "마" };
+
+            foreach (string to in strarr)
+            {
+                Console.WriteLine($"to : {to}");
+            }
+            //주의사항 : string은 int, double 처럼 사용함(값타입처럼)
+
+            string str = "가나다라마가나";
+
+            int loc = str.IndexOf("가");
+            Console.WriteLine(loc);
+
+            int lastloc = str.LastIndexOf("가");
+            Console.WriteLine(lastloc);
+
+            string strinsert = str.Insert(2, "가");
+            Console.WriteLine(strinsert);
+
+            string strremove = str.Remove(2, 2);
+            Console.WriteLine(strremove);
+
+            string strreplace = str.Replace("가", "H");
+            Console.WriteLine(strreplace);
+
+            string strsubstring = str.Substring(2, 1);
+            Console.WriteLine(strsubstring);
+
+            //문제
+            string super = "슈퍼맨/팬티.노랑색/우하하";
+            string[] subs = super.Split('/', '.');
+            foreach (string i in subs)
+            {
+                Console.WriteLine(i);
+            }
+
+            char[] ch = new char[] { '/', '.', '-' };
+
+            string[] strarray = super.Split(ch);
+            foreach (string s in strarray)
+            {
+                Console.WriteLine(s);
+            }
+
+            int sum = 0;
+            string[] numarr = { "1", "2", "3", "4", "5" };
+            // 문제1) 배열에 있는 문자값들의 합을 sum 변수에 담아서 출력
+            foreach (string i in numarr)
+            {
+                sum += Convert.ToInt32(i);
+
+            }
+            Console.WriteLine(sum);
+
+            string Jumin = "123456-1234567";
+            int sum_1 = 0;
+            //문제 2) 주민번호의 합
+            for (int i = 0; i < Jumin.Length; i++)
+            {
+                if (Jumin[i] == '-')
+                {
+                    continue;
+                }
+                else
+                {
+                    sum_1 += int.Parse(Convert.ToString(Jumin[i]));
+                }
+            }
+            Console.WriteLine(sum_1);
+
+
+        }
+    }
+}
---
+++ Ex25_String_function_Quiz/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex26_String_function_Quiz
+{
+    //주민번호 : 뒷번호 첫자리 : 1,3 남자 , 2,4 여자 라고 출력 ... 하는 문제
+
+    //main 함수    주민번호 입력받고
+    //앞:6자리     뒷:7자리
+    //입력값 : 123456-1234567 
+
+    //함수 3개를 static 
+    //1. 자리수 체크 (기능)함수 (14 ok)  return true , false 
+    //2. 뒷번호 첫번째 자리값 1~4까지의 값만 허용 기능함수  return true, false
+    //3. 뒷번호 첫번째 자리값을 가지고 1,3 남자 , 2,4 여자 출력 기능함수  void  출력
+
+    class Program
+    {
+        static void juminDisplay(string ssn)
+        {
+            bool first = juminFirstNumber(ssn);
+            int first_num;
+
+            if (first == true)
+            {
+                first_num = int.Parse(Convert.ToString(ssn.Substring(7, 1)));
+                Console.WriteLine(((first_num == 1) || (first_num == 3)) ? "남자" : "여자");
+            }
+            else
+            {
+                Console.WriteLine("허용되지 않은 주민번호 입니다.");
+            }
+        }
+
+        static bool juminFirstNumber(string str)
+        {
+            int first;
+            bool check;
+            check = juminCheck(str);
+
+            first = (check == true) ? int.Parse(Convert.ToString(str.Substring(7, 1))) : 0;
+
+            return ((first > 0) && (first <= 4) && (check == true)) ? true : false; ;
+        }
+
+        static bool juminCheck(string str)
+        {
+            return str.Length == 14 ? true : false;
+        }
+
+        static void Main(string[] args)
+        {
+            string jumin;
+            Console.Write("주민번호를 입력하세요 : ");
+            jumin = Console.ReadLine();
+            juminDisplay(jumin);
+
+        }
+
+    }
+}
