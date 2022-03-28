840aac0fea89fd1789f0dfa361b7df76882c2a73
MINSEONG KIM <58250610+minzzy524@users.noreply.github.com>
2022-03-28 15:25:06 +0900

    2022.03.28 15:30

---
+++ homework1/Abstract Factory Pattern/Abstract Factory Pattern_3_2.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public interface IBody
+    {
+
+    }
+
+    public interface IString
+    {
+
+    }
+
+    public class AcousticBody : IBody
+    {
+
+        public AcousticBody()
+        {
+            Console.WriteLine("통기타의 몸체입니다\n");
+        }
+    }
+    public class ElectricBody : IBody
+    {
+
+        public ElectricBody()
+        {
+            Console.WriteLine("일렉기타의 몸체입니다.\n");
+        }
+    }
+    public class AcousticString : IString
+    {
+
+        public AcousticString()
+        {
+            Console.WriteLine("통기타의 줄입니다\n");
+        }
+    }
+    public class ElectricString : IString
+    {
+
+        public ElectricString()
+        {
+            Console.WriteLine("일렉기타의 줄입니다.\n");
+        }
+    }
+
+
+    public interface IGuitarFactory
+    {
+
+        void make();
+        IBody CreateBody();
+        IString CreateString();
+
+    }
+
+    public class AcousticGuitarFactory : IGuitarFactory
+    {
+
+        public void make()
+        {
+            Console.WriteLine("통기타 제작 재료입니다\n");
+        }
+        public IBody CreateBody()
+        {
+            return new AcousticBody();
+        }
+        public IString CreateString()
+        {
+            return new AcousticString();
+        }
+    }
+
+    public class ElectricGuitarFactory : IGuitarFactory
+    {
+        public void make()
+        {
+            Console.WriteLine("일렉기타 제작 재료입니다\n");
+        }
+        public IBody CreateBody()
+        {
+            return new ElectricBody();
+        }
+        public IString CreateString()
+        {
+            return new ElectricString();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGuitarFactory acuostic = new AcousticGuitarFactory();
+            IGuitarFactory electric = new ElectricGuitarFactory();
+
+            acuostic.make();
+            acuostic.CreateBody();
+            acuostic.CreateString();
+
+            electric.make();
+            electric.CreateBody();
+            electric.CreateString();
+
+        }
+    }
+}
